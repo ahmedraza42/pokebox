2176df82f90b0efb0043c2e9e41f34d2da388c47
Ahmedraza42 <ahmedraza6942@gmail.com>
2023-08-14 19:34:46 +0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import 'react-native-gesture-handler';
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import Home from './src/screens/Home';
+import { RootSiblingParent } from 'react-native-root-siblings';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: Colors.darker 
+  };
+
+  return (
+    <RootSiblingParent>
+
+   
+    <SafeAreaView style={{flex:1}}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Home/>
+    
+    </SafeAreaView>
+    </RootSiblingParent>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
